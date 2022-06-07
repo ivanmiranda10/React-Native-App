6f849bcb1c8cd6f4dab9d50dfa3063c8a5c071af
Ivan Miranda <gonzalobarnichea10@gmail.com>
2022-06-07 16:40:45 -0300

    Client Done

---
+++ client/src/Components/Home/index.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { View, SafeAreaView, FlatList } from "react-native";
+import { COLORS, NFTData } from "../../../constants";
+import {
+  NFTCard,
+  HomeHeader,
+  FocusedStatusBar,
+} from "../../ReusableComponents";
+import axios from "axios";
+
+const Home = () => {
+  const [nftData, setNftData] = useState(NFTData);
+
+  const [apiData, setApiData] = useState(null);
+
+  const getApiInfo = async () => {
+    const resp = await axios.get("http://192.168.0.19:3001");
+    console.log(resp.data);
+    setApiData(resp.data.slice(5, 6));
+  };
+
+  useEffect(() => {
+    getApiInfo();
+  }, []);
+
+  const handleSearch = (value) => {
+    if (value.length === 0) {
+      setNftData(NFTData);
+    }
+
+    const filteredData = NFTData.filter((item) =>
+      item.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    if (filteredData.length === 0) {
+      setNftData(NFTData);
+    } else {
+      setNftData(filteredData);
+    }
+  };
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar background={COLORS.primary} />
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={nftData}
+            renderItem={({ item }) => <NFTCard data={item} apiData={apiData} />}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
+          />
+        </View>
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            zIndex: -1,
+          }}
+        >
+          <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+          <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
---
+++ client/src/ReusableComponents/index.jsx
@@ -0,0 +1,18 @@
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import FocusedStatusBar from "./focusedStatusBar";
+import { CircleButton, RectButton } from "./Buttons";
+import DetailDesc from "./DetailDesc";
+import DetailBid from "./DetailBid";
+import { SubInfo } from "./SubInfo";
+
+export {
+  HomeHeader,
+  NFTCard,
+  FocusedStatusBar,
+  CircleButton,
+  RectButton,
+  DetailDesc,
+  DetailBid,
+  SubInfo,
+};
